e23abc087086e12047dae70b2c818e4093512cd2
killkirill <nepstor7@gmail.com>
2022-11-05 18:32:36 +0200

    Add schedule running via cron

--- bot.js
+++ bot.js
@@ -2,29 +2,35 @@ const { CONFIGS } = require("./configs/configs");
 const { RESERVE_VISIT_LINK } = require("./requests/requests");
 const { getListOfFreeDates, getListOfFreeTimes, notifyViaEmail } = require("./functions/functions");
 const { getNearestFreeDateInUTC, dateDifferenceFromCurrentDateInDays, getNearestFreeDateStringForApiRequest } = require("./utils/dateUtils");
+const cronJob = require('cron').CronJob;
 
-async function checkForFreeDates() {
-    const dates = await getListOfFreeDates();
-    const nearestFreeDateInUTC = getNearestFreeDateInUTC(dates);
-    const daysTillVisit = dateDifferenceFromCurrentDateInDays(nearestFreeDateInUTC);
-    const visitInDaysMessage = `The nearest Free Date to visit in ${daysTillVisit} days`
+const job = new cronJob(
 
-    if (CONFIGS.DAYS_TO_CHECK >= daysTillVisit) {
-        const nearestFreeDateForApiRequest = getNearestFreeDateStringForApiRequest(dates);
-        const listOfFreeTimes = await getListOfFreeTimes(nearestFreeDateForApiRequest);
+    CONFIGS.SCHEDULE_OF_CHECK,
 
-        const emailSubject = "Notification - Free slots to visit LT MIGRIS";
-        const emailText = `${visitInDaysMessage}.
-            \n${nearestFreeDateForApiRequest} at ${CONFIGS.ADDRESS}.
-            \nAvailable times: \n${listOfFreeTimes};
-            \nThe link to register visit: \n${RESERVE_VISIT_LINK}`;
-        console.log(emailText);
+	async function checkForFreeDates() {
+        const dates = await getListOfFreeDates();
+        const nearestFreeDateInUTC = getNearestFreeDateInUTC(dates);
+        const daysTillVisit = dateDifferenceFromCurrentDateInDays(nearestFreeDateInUTC);
+        const visitInDaysMessage = `The nearest Free Date to visit in ${daysTillVisit} days`
 
-        await notifyViaEmail(CONFIGS.EMAIL_OF_RECEIVER, emailSubject, emailText);
+        if (CONFIGS.DAYS_TO_CHECK >= daysTillVisit) {
+            const nearestFreeDateForApiRequest = getNearestFreeDateStringForApiRequest(dates);
+            const listOfFreeTimes = await getListOfFreeTimes(nearestFreeDateForApiRequest);
 
-    } else {
-        console.log(`Sorry, there are no available dates for registration in ${CONFIGS.DAYS_TO_CHECK} days. ${visitInDaysMessage}.`)
+            const emailSubject = "Notification - Free slots to visit LT MIGRIS";
+            const emailText = `${visitInDaysMessage}.
+                \n${nearestFreeDateForApiRequest} at ${CONFIGS.ADDRESS}.
+                \nAvailable times: \n${listOfFreeTimes};
+                \nThe link to register visit: \n${RESERVE_VISIT_LINK}`;
+            console.log(emailText);
+
+            await notifyViaEmail(CONFIGS.EMAIL_OF_RECEIVER, emailSubject, emailText);
+
+        } else {
+            console.log(`Sorry, there are no available dates for registration in ${CONFIGS.DAYS_TO_CHECK} days. ${visitInDaysMessage}.`)
+        }
     }
-}
+);
 
-checkForFreeDates();
+job.start()
